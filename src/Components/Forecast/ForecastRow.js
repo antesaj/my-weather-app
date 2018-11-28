1db1d31bd166b8771c184aa2c1281ae94f493220
Andrew Antes <antesaj@gmail.com>
2018-11-28 21:01:29 +0000

    styled the forecast block a bit.

--- src/Components/Forecast/ForecastRow.js
+++ src/Components/Forecast/ForecastRow.js
@@ -8,14 +8,27 @@ class ForecastRow extends Component {
     }
     
     render() {
+        
+        const forecast = this.props.forecast;
+        
+        const weather = {
+            lowTempF: forecast.day.mintemp_f,
+            hiTempF: forecast.day.maxtemp_f,
+            precipInch: forecast.day.totalprecip_in
+        };
+        
         return (
           
             <tr className="row">
                 <td>
                     <span className="day">{this.props.day}</span>
                 </td>
-                <td>
-                    <span className="temp">{this.props.forecast.day.avgtemp_f}</span>
+                <td className="temp">
+                    <span className="rng">
+                        <span className="loTemp">{weather.lowTempF}&deg;F</span>
+                        <span className="bar"></span>
+                        <span className="hiTemp">{weather.hiTempF}&deg;F</span>
+                    </span>
                 </td>
             </tr>
           
